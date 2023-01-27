b6281fe83f8b182bae9f8f72a37d1266c6833a64
Nick Keepfer <45045579+nickkeepfer@users.noreply.github.com>
2023-01-27 15:44:33 +0000

    Update src/MarchingCubes.jl
    Co-authored-by: t-bltg <tf.bltg@gmail.com>

--- src/MarchingCubes.jl
+++ src/MarchingCubes.jl
@@ -640,9 +640,9 @@ end
 add_z_vertex(m::MC{F}, vol, cb, i, j, k) where {F} = begin
     @inbounds begin
         u = cb[1] / (cb[1] - cb[5])
-        m.nrm[1] = m.normal_sign * (1 - u) * ∇x(vol, i, j, k, m.nx) + u * ∇x(vol, i, j, k + 1, m.nx)
-        m.nrm[2] = m.normal_sign * (1 - u) * ∇y(vol, i, j, k, m.ny) + u * ∇y(vol, i, j, k + 1, m.ny)
-        m.nrm[3] = m.normal_sign * (1 - u) * ∇z(vol, i, j, k, m.nz) + u * ∇z(vol, i, j, k + 1, m.nz)
+        m.nrm[1] = (1 - u) * ∇x(vol, i, j, k, m.nx) + u * ∇x(vol, i, j, k + 1, m.nx)
+        m.nrm[2] = (1 - u) * ∇y(vol, i, j, k, m.ny) + u * ∇y(vol, i, j, k + 1, m.ny)
+        m.nrm[3] = (1 - u) * ∇z(vol, i, j, k, m.nz) + u * ∇z(vol, i, j, k + 1, m.nz)
         (mag = norm(m.nrm)) > eps(F) && (m.nrm ./= mag)
     end
     push!(m.vertices, Vertex(i - 1, j - 1, k - 1 + u))
