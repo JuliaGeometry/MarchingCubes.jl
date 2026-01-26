46c2d4bff4c1761bcbc440e704323a219cbeb300
t-bltg <tf.bltg@gmail.com>
2026-01-26 12:37:40 +0100

    format

--- src/example.jl
+++ src/example.jl
@@ -1,13 +1,13 @@
 @inline cb_cushin(x, y, z) = (
     z^2 * x^2 - z^4 - 2z * x^2 + 2z^3 + x^2 - z^2 - (x^2 - z)^2 - y^4 - 2x^2 * y^2 -
-    y^2 * z^2 +
-    2y^2 * z +
-    y^2
+        y^2 * z^2 +
+        2y^2 * z +
+        y^2
 )
 @inline cb_sphere(x, y, z) = (
     ((x - 2)^2 + (y - 2)^2 + (z - 2)^2 - 1) *
-    ((x + 2)^2 + (y - 2)^2 + (z - 2)^2 - 1) *
-    ((x - 2)^2 + (y + 2)^2 + (z - 2)^2 - 1)
+        ((x + 2)^2 + (y - 2)^2 + (z - 2)^2 - 1) *
+        ((x - 2)^2 + (y + 2)^2 + (z - 2)^2 - 1)
 )
 @inline cb_plane(x, y, z) = x + y + z - 3
 @inline cb_cassini(x::F, y::F, z::F) where {F} =
@@ -20,17 +20,17 @@
     (x^2 + y^2 + z^2 + b^2 - d^2)^2 - 4((a * x - c * d)^2 + b^2 * y^2)
 @inline cb_torus2(x::F, y::F, z::F, r = F(1.85), R = F(4)) where {F} = (
     ((x^2 + y^2 + z^2 + R^2 - r^2)^2 - 4R^2 * (x^2 + y^2)) *
-    ((x^2 + (y + R)^2 + z^2 + R^2 - r^2)^2 - 4R^2 * ((y + R)^2 + z^2))
+        ((x^2 + (y + R)^2 + z^2 + R^2 - r^2)^2 - 4R^2 * ((y + R)^2 + z^2))
 )
 @inline cb_mc_case(x::F, y::F, z::F) where {F} = (
     F(-26.5298) * (1 - x) * (1 - y) * (1 - z) +
-    F(+81.9199) * x * (1 - y) * (1 - z) +
-    F(-100.680) * x * y * (1 - z) +
-    F(+3.54980) * (1 - x) * y * (1 - z) +
-    F(+24.1201) * (1 - x) * (1 - y) * z +
-    F(-74.4702) * x * (1 - y) * z +
-    F(+91.5298) * x * y * z +
-    F(-3.22998) * (1 - x) * y * z
+        F(+81.9199) * x * (1 - y) * (1 - z) +
+        F(-100.68) * x * y * (1 - z) +
+        F(+3.5498) * (1 - x) * y * (1 - z) +
+        F(+24.1201) * (1 - x) * (1 - y) * z +
+        F(-74.4702) * x * (1 - y) * z +
+        F(+91.5298) * x * y * z +
+        F(-3.22998) * (1 - x) * y * z
 )
 @inline cb_drip(x::F, y::F, z::F, a = 2, b = 2, c = 3, d = 6) where {F} =
     x^2 + y^2 - F(0.5) * (F(0.995) * z^2 + F(0.005) - z^3) + F(0.0025)
@@ -44,7 +44,7 @@ fill_volume!(vol::AbstractArray{F}, cb::Function) where {F} = begin
     ty = F(ny / 2sy + 1.5)
     tz = F(nz / 2sz)
 
-    @inbounds for k ∈ 1:nz, j ∈ 1:ny, i ∈ 1:nx
+    @inbounds for k in 1:nz, j in 1:ny, i in 1:nx
         vol[i, j, k] = cb(F((i - 1) / sx - tx), F((j - 1) / sy - ty), F((k - 1) / sz - tz))
     end
 
@@ -85,10 +85,10 @@ end
 
 output(
     PlyIO::Module,
-    m::MC{F,I},
+    m::MC{F, I},
     fn::AbstractString = "test.ply";
     verbose = true,
-) where {F,I} = begin
+) where {F, I} = begin
     nv, nt = length(m.vertices), length(m.triangles)
     verbose && println("Writing $nv vertices and $nt triangles using `PlyIO`.")
 
@@ -107,7 +107,7 @@ output(
     )
 
     vertex_indices = PlyIO.ListProperty("vertex_indices", I, eltype(Triangle))
-    for i ∈ eachindex(m.triangles)
+    for i in eachindex(m.triangles)
         push!(vertex_indices, m.triangles[i] .- 1)  # 1-based indexing -> 0-based indexing
     end
     push!(ply, PlyIO.PlyElement("face", vertex_indices))
@@ -130,10 +130,10 @@ makemesh_GeometryBasics(GeometryBasics::Module, m::MC) = begin
 end
 
 makemesh(mod::Module, m::MC) =
-    if (mod_str = string(mod)) == "Meshes"
-        makemesh_Meshes(mod, m)
-    elseif mod_str == "GeometryBasics"
-        makemesh_GeometryBasics(mod, m)
-    else
-        throw(ArgumentError("un-supported module `$mod`"))
-    end
+if (mod_str = string(mod)) == "Meshes"
+    makemesh_Meshes(mod, m)
+elseif mod_str == "GeometryBasics"
+    makemesh_GeometryBasics(mod, m)
+else
+    throw(ArgumentError("un-supported module `$mod`"))
+end
